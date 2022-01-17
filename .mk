837de573998099e8452227eacc11c9100523f66e
Paul Handy <paul@pauldhandy.com>
2022-01-17 13:04:09 -0800

    Added OVMF binaries

--- .mk
+++ .mk
@@ -33,8 +33,8 @@ all: $(BINDIR)/$(TARGET)
 
 qemu: all
 	qemu-system-x86_64 -enable-kvm \
-	-drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd \
-	-drive if=pflash,format=raw,file=OVMF_VARS.fd \
+	-drive if=pflash,format=raw,readonly=on,file=./ovmf/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,file=./ovmf/OVMF_VARS.fd \
 	-drive index=0,format=raw,file=fat:rw:$(BINDIR)
 	#-nographic \
 
